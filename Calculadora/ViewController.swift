ca6d122eded7c4d88bbee5b2056bdf0f8c7e07aa
DAM <DAM@iMac.local>
2017-10-25 21:13:03 +0200

    EMPEZADA CALCULADORA

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -9,17 +9,115 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var lastTotal = 0;
+    var lasFirst = 0;
+    var lastSecond = 0;
+    var lastSign = "";
+    
+    var total = 0.0;
+    var first = 0.0;
+    var second = 0.0;
+    var sign = "";
+    var show = "";
+    
+    var who = false;
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func clear(_ sender: Any) {
+        lastTotal = 0;
+        lasFirst = 0;
+        lastSecond = 0;
+        lastSign = "";
+        
+        total = 0.0;
+        first = 0.0;
+        second = 0.0;
+        sign = "";
+        show = "";
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func total(_ sender: Any) {
+        
     }
+    
+    @IBAction func whatDoes(_ sender: Any) {
+        switch (sender as AnyObject).tag {
+        case 0:
+            
+        case 1:
+            
+        case 2:
+            
+        case 3:
+            
+        default:
+            
+        }
+    }
+    @IBAction func numClick(_ sender: Any) {
+        if !who {
+            
+            first = findNumber(sender: sender as AnyObject)
+            
+            show.append(String(first));
+            
+        }else{
+            second = findNumber(sender: sender as AnyObject)
+            
+            show.append(String(second));
+        }
+    }
+    
+    override func viewDidLoad() {super.viewDidLoad()}
+
+    override func didReceiveMemoryWarning() {super.didReceiveMemoryWarning()}
 
 
+    func suma(first:Double, second:Double){
+        first+second
+    }
+    func resta(first:Double, second:Double){
+        first-second
+    }
+    func mult(first:Double, second:Double){
+        first*second
+    }
+    func div(first:Double, second:Double){
+        first/second
+    }
+    
+    func findNumber(sender:AnyObject) -> Double{
+        
+        switch sender.tag {
+        case 0:
+            return 0.0;
+        case 1:
+            return 1.0;
+        case 2:
+            return 2.0;
+        case 3:
+            return 3.0;
+        case 4:
+            return 4.0;
+        case 5:
+            return 5.0;
+        case 6:
+            return 6.0;
+        case 7:
+            return 7.0;
+        case 8:
+            return 8.0;
+        case 9:
+            return 9.0;
+        default:
+            return 0.0
+        }
+    }
+    
+    func setText(){
+        
+    }
 }
 
